44ef543619a245b08e9f4b0f294f7d3a261a7e59
Tirath Ramdas <tirath@int19h.com>
2014-08-22 14:44:09 +1000

    Document some things

--- whose_mem.c
+++ whose_mem.c
@@ -9,6 +9,45 @@
 #include <sys/sysctl.h>
 #include <sys/kern_memorystatus.h>
 
+
+/*
+ * This is what the vm_statistics structure looks like in xnu-2422.1.72 :
+ */
+//struct vm_statistics64 {
+//	natural_t	free_count;		/* # of pages free */
+//	natural_t	active_count;		/* # of pages active */
+//	natural_t	inactive_count;		/* # of pages inactive */
+//	natural_t	wire_count;		/* # of pages wired down */
+//	uint64_t	zero_fill_count;	/* # of zero fill pages */
+//	uint64_t	reactivations;		/* # of pages reactivated */
+//	uint64_t	pageins;		/* # of pageins */
+//	uint64_t	pageouts;		/* # of pageouts */
+//	uint64_t	faults;			/* # of faults */
+//	uint64_t	cow_faults;		/* # of copy-on-writes */
+//	uint64_t	lookups;		/* object cache lookups */
+//	uint64_t	hits;			/* object cache hits */
+//	uint64_t	purges;			/* # of pages purged */
+//	natural_t	purgeable_count;	/* # of pages purgeable */
+//	/*
+//	 * NB: speculative pages are already accounted for in "free_count",
+//	 * so "speculative_count" is the number of "free" pages that are
+//	 * used to hold data that was read speculatively from disk but
+//	 * haven't actually been used by anyone so far.
+//	 */
+//	natural_t	speculative_count;	/* # of pages speculative */
+//
+//	/* added for rev1 */
+//	uint64_t	decompressions;		/* # of pages decompressed */
+//	uint64_t	compressions;		/* # of pages compressed */
+//	uint64_t	swapins;		/* # of pages swapped in (via compression segments) */
+//	uint64_t	swapouts;		/* # of pages swapped out (via compression segments) */
+//	natural_t	compressor_page_count;	/* # of pages used by the compressed pager to hold all the compressed data */
+//	natural_t	throttled_count;	/* # of pages throttled */
+//	natural_t	external_page_count;	/* # of pages that are file-backed (non-swap) */
+//	natural_t	internal_page_count;	/* # of pages that are anonymous */
+//	uint64_t	total_uncompressed_pages_in_compressor; /* # of pages (uncompressed) held within the compressor. */
+//} __attribute__((aligned(8)));
+
 static unsigned int read_sysctl_int(const char* name)
 {
     unsigned int var;
@@ -82,6 +121,7 @@ static int get_percent_free(unsigned int* level)
 {
     int error;
 
+    // This is how AAPL's memory_pressure tool reports "System-wide memory free percentage":
     error = memorystatus_get_level((user_addr_t) level);
 
     if( error ) {
